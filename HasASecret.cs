cb4110f3e8f19ac0cd824ffd9ae6da2f7656f15f
Eugene Volkov <bizzar@list.ru>
2021-03-09 19:47:27 +0300

    Add project files.

---
+++ HasASecret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_HasASecret_Console
+{
+    class HasASecret
+    {
+        // This class has a secret field. Does the private keyword make it sure?
+        private string secret = "xyzzy";
+    }
+}
